c2248f86cab60a4cb71fbc7153faa11474e404ef
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:25:25 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet build magic.lambda.validators/magic.lambda.validators.csproj --configuration Release
+dotnet build magic.lambda.validators/magic.lambda.validators.csproj --configuration Release --source https://api.nuget.org/v3/index.json
